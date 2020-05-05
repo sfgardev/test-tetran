ce947bb35453e362cc99509c3a279db4b8ecfeef
sfgardev <sf.garol@gmail.com>
2020-05-05 14:28:21 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+
+  $(".phone").css({"font-size": "10px"});
+  $(".operator").css({"display": "none"})
+
+  $(".time").css({"border": "1px solid pink"}, {"display": "inline"});
+  $(".green").css({"color": "green"});
+
+});
